bf6dfad0d30c85e7c014312689f39dfe7f72cb98
Agnes Crepet <agnes.crepet@gmail.com>
2013-11-10 21:56:50 +0100

    correction date bis

--- src/main/java/com/ninja_squad/craftsmanplan/Schedule.java
+++ src/main/java/com/ninja_squad/craftsmanplan/Schedule.java
@@ -6,7 +6,16 @@ import java.io.IOException;
 import java.util.*;
 
 public class Schedule {
-    private List<Appointment> appointmentList = new LinkedList<Appointment>();
+    private Set<Appointment> appointmentList = new TreeSet<Appointment>(new Comparator<Appointment>() {
+        @Override
+        public int compare(Appointment app1, Appointment app2) {
+            if (app1.getBeginning().isBefore(app2.getBeginning()))
+                return -1;
+            if (app2.getBeginning().isBefore(app1.getBeginning()))
+                return 1;
+            return 0;
+        }
+    });
 
     public void addAppointment(Appointment appointment) {
         appointmentList.add(appointment);
